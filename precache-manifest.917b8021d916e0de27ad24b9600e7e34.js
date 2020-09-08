e8521255ec73c2b4a1d084aebf76837eedef7ada
Burhan <mr47.thehacker@hotmail.com>
2020-09-08 13:20:50 +0500

    Updates

---
+++ precache-manifest.917b8021d916e0de27ad24b9600e7e34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64daeffbbc75e60c2bae1757790cbb1d",
+    "url": "/burhan/index.html"
+  },
+  {
+    "revision": "1b23af11a15b181be0c3",
+    "url": "/burhan/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "92e04bfc0e72b434f826",
+    "url": "/burhan/static/js/2.034578d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burhan/static/js/2.034578d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b23af11a15b181be0c3",
+    "url": "/burhan/static/js/main.45a44028.chunk.js"
+  },
+  {
+    "revision": "a1e5916c31f4775f31c6",
+    "url": "/burhan/static/js/runtime-main.20a4c57b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/burhan/static/media/logo.5d5d9eef.svg"
+  }
+]);
